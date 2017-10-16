f5d6f330ac2dfb9de086f6d04b3e0113c2395a38
Israel Davila <idavilam1100@alumno.ipn.mx>
2017-10-16 00:14:13 -0500

    Enable GPIOA

--- source/main.c
+++ source/main.c
@@ -1,6 +1,8 @@
 #include "stm32f0xx_ll_gpio.h"
+#include "stm32f0xx_ll_bus.h"
 
 int main(){
+  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA);
   LL_GPIO_InitTypeDef mygpio;
   LL_GPIO_StructInit(&mygpio);
   return 0;
