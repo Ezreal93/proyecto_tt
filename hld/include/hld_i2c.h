7f5de7f66d55bb0e704bdeb12f4544fdf6f6ae24
Manuel Limones <limonesu.me@gmail.com>
2017-10-19 04:58:15 -0500

    Add read and write function for i2c1

--- hld/include/hld_i2c.h
+++ hld/include/hld_i2c.h
@@ -1,6 +1,10 @@
 #ifndef HDL_I2C_H
 #define HDL_I2C_H
 
+#include <stdint.h>
+
 void init_i2c1(void);
+int i2c1_write(uint8_t address, uint8_t nbytes, uint8_t *data);
+int i2c1_read(uint8_t address, uint8_t internal_address, uint8_t nbytes, uint8_t *data);
 
 #endif
--- hld/source/hld_i2c.c
+++ hld/source/hld_i2c.c
@@ -1,5 +1,7 @@
 #include "hld_i2c.h"
 
+#include <stdbool.h>
+
 #include "stm32f0xx_ll_gpio.h"
 #include "stm32f0xx_ll_bus.h"
 #include "stm32f0xx_ll_i2c.h"
@@ -22,3 +24,81 @@ void init_i2c1(void){
     LL_I2C_Init(I2C1, &i2c);
     LL_I2C_Enable(I2C1);
  }
+
+static uint8_t bytes_remaining = 0;
+static uint8_t *p_data;
+static uint8_t transfer_in_progress = 0;
+
+int i2c1_write(uint8_t address, uint8_t nbytes, uint8_t *data){
+    LL_I2C_SetSlaveAddr(I2C1, address << 1);
+    LL_I2C_SetTransferSize(I2C1, nbytes);
+    LL_I2C_EnableAutoEndMode(I2C1);
+    LL_I2C_SetTransferRequest(I2C1,  LL_I2C_REQUEST_WRITE);
+
+    bytes_remaining = nbytes;
+    p_data = data;
+    transfer_in_progress = true;
+
+
+    LL_I2C_EnableIT_TX(I2C1);
+    NVIC_EnableIRQ(I2C1_IRQn);
+
+    LL_I2C_GenerateStartCondition(I2C1);
+
+    while(transfer_in_progress){};
+
+    return 0;
+}
+
+int i2c1_read(uint8_t address, uint8_t internal_address, uint8_t nbytes, uint8_t *data){
+    LL_I2C_SetSlaveAddr(I2C1, address << 1);
+    LL_I2C_SetTransferSize(I2C1, 1);
+    LL_I2C_DisableAutoEndMode(I2C1);
+    LL_I2C_SetTransferRequest(I2C1,  LL_I2C_REQUEST_WRITE);
+
+    bytes_remaining = nbytes;
+    p_data = data;
+    transfer_in_progress = true;
+
+    while(!LL_I2C_IsActiveFlag_TXIS(I2C1));
+    LL_I2C_TransmitData8(I2C1, internal_address);
+
+    while(!LL_I2C_IsActiveFlag_TC(I2C1));
+    LL_I2C_GenerateStartCondition(I2C1);
+    LL_I2C_SetTransferSize(I2C1, nbytes);
+    LL_I2C_EnableAutoEndMode(I2C1);
+    LL_I2C_SetTransferRequest(I2C1,  LL_I2C_REQUEST_READ);
+
+    LL_I2C_EnableIT_RX(I2C1);
+    NVIC_EnableIRQ(I2C1_IRQn);
+
+    LL_I2C_GenerateStartCondition(I2C1);
+
+    while(transfer_in_progress){};
+
+    return 0;
+}
+
+
+void I2C1_IRQHandler(void){
+    if(LL_I2C_IsActiveFlag_TXIS(I2C1)){
+        LL_I2C_TransmitData8(I2C1, *p_data);
+        p_data++;
+        bytes_remaining--;
+        if(!bytes_remaining){
+            transfer_in_progress = false;
+            LL_I2C_DisableIT_TX(I2C1);
+            NVIC_DisableIRQ(I2C1_IRQn);
+        }
+    }
+    if(LL_I2C_IsActiveFlag_RXNE(I2C1)){
+        *p_data = LL_I2C_ReceiveData8(I2C1);
+        p_data++;
+        bytes_remaining--;
+        if(!bytes_remaining){
+            transfer_in_progress = false;
+            LL_I2C_DisableIT_RX(I2C1);
+            NVIC_DisableIRQ(I2C1_IRQn);
+        }
+    }
+}
